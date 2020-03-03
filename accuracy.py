d9923f3fdc1b49e5f8cd26ef29fe063e56d1689c
Robert Minahan <minahanr75@gmail.com>
2020-03-02 23:45:53 -0800

    updated accuracy.py to graph something else and made load_csv more efficient

--- accuracy.py
+++ accuracy.py
@@ -1,26 +1,32 @@
 import numpy as np
 import csv #for loading
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
-def load_csv(filename):
-    file = open(filename, 'r')
-    lines = csv.reader(file)
-    dataset = list(lines)
-    return dataset
+def load_csv(filename, sep=',', training_indices=(20,-20), runningTest=True):
+    csv = []
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.split(sep)
+
+            if runningTest == True:
+                del line[training_indices[0]: training_indices[1]]
+            else:
+                line = line[training_indices[0]: training_indices[1]]
+                
+            csv.append([float(i) for i in line])
+
+    return np.array(csv, dtype=np.float64)
 
 def main():
     #import as numpy array which is really useful for matrix multiplication, etc.
-    predictions = np.array(load_csv('C:\\Users\\minah\\OneDrive\\Documents\\UCLA\\Junior Year\\Winter\\CASB_185\\predictions.csv'), dtype=np.float64)
-    last20 = predictions[:, -20:]
-    first20 = predictions[:, :20]
-    predictions = np.append(first20, last20, axis=1)
+    print('testing')
+    predictions = load_csv('predictions.csv')
     print('Finished loading predictions')
     
-    methylation_data = np.array(load_csv('C:\\Users\\minah\\OneDrive\\Documents\\UCLA\\Junior Year\\Winter\\CASB_185\\methylation_data_no_labels.csv'), dtype=np.float64)
-    last20 = methylation_data[:, -20:]
-    first20 = methylation_data[:, :20]
-    methylation_data = np.append(first20, last20, axis=1)
+    methylation_data = load_csv('methylation_data_no_labels.csv')
+
     print('Finished loading methylation_data')
 
     difference = np.absolute(np.subtract(methylation_data, predictions))
@@ -30,28 +36,32 @@ def main():
 
     for i in range(error_margin.shape[0]): # number of rows
     	for j in range(error_margin.shape[1]): # number of cols
-        	if error_margin[i,j] > 0.1: # 10% margin of error
-        		num_flags += 1
+            if error_margin[i,j] > 0.1: # 10% margin of error
+                if error_margin[i,j] > 1:
+                    print(methylation_data[i,j], error_margin[i, j])
+                num_flags += 1
     	if i%100000 == 0:
-        	print i
-        
-	print num_flags/float(error_margin.shape[0]*error_margin.shape[1]) # cast denominator to float so that result is float
+            print(i)
 
-	errors_by_site = np.mean(error_margin, axis=1)
+    print(num_flags / float(error_margin.shape[0] * error_margin.shape[1]))
 
-	plt.scatter(range(errors_by_site.shape[0]), errors_by_site)
-	plt.show()
+    errors_by_site = np.mean(error_margin, axis=1)
 
-	true_values = np.mean(methylation_data, axis=1)
-	predicted_values = np.mean(predictions, axis=1)
+    plt.scatter(range(errors_by_site.shape[0]), errors_by_site)
+    plt.show()
+    
+    true_values = np.mean(methylation_data, axis=1)
+    predicted_values = np.mean(predictions, axis=1)
 
-	predicted_values_above = predicted_values + np.std(predictions, axis=1)
-	predicted_values_below = predicted_values - np.std(predictions, axis=1)
+    predicted_values_above = predicted_values + np.std(predictions, axis=1)
+    predicted_values_below = predicted_values - np.std(predictions, axis=1)
 
-	plt.scatter(list(true_values), list(predicted_values))
-	plt.scatter(list(true_values), list(predicted_values_above))
-	plt.scatter(list(true_values), list(predicted_values_below))
-	plt.show()
+    print(r2_score(true_values, predicted_values))
+    
+    plt.scatter(list(true_values), list(predicted_values_above))
+    plt.scatter(list(true_values), list(predicted_values_below))
+    plt.scatter(list(true_values), list(predicted_values))
+    plt.show()
 
 if __name__ == '__main__':
-	main()
+    main()
