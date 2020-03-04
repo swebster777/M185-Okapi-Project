b1432651e294dc9a381043bcbcd2bf56988c0b27
swebster777 <53287866+swebster777@users.noreply.github.com>
2020-03-04 01:08:32 -0800

    Update linear_regression_output_coefficients.py

--- linear_regression_output_coefficients.py
+++ linear_regression_output_coefficients.py
@@ -1,33 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import csv
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+
+
+# In[2]:
+
 
 def load_csv(filename):
-	file = open(filename, 'r')
-	lines = csv.reader(file)
-	dataset = list(lines)
-	return dataset
+    file = open(filename, 'r')
+    lines = csv.reader(file)
+    dataset = list(lines)
+    return dataset
 
-def main():
-	#import methylation
-	#import meta data
-	meta_data = np.array(load_csv('meta_data_no_labels.csv'), dtype=np.float64)[20:-20, :]
-	#transpose meta data
-	methylation_data = np.array(load_csv('methylation_data_no_labels.csv'), dtype=np.float64)[:, 20:-20]
-	
-	coefficient_matrix = []
+
+# In[3]:
+
+
+#import methylation
+#import meta data
+meta_data = np.array(load_csv('meta_data_no_labels_6var.csv'), dtype=np.float64)[20:-20, :]
+#transpose meta data
+methylation_data = np.array(load_csv('methylation_data_no_labels.csv'), dtype=np.float64)[:, 20:-20]
+
+
+# In[8]:
+
+
+print meta_data[:20]
+print methylation_data[:20]
+
+
+# In[4]:
+
+
+coefficient_matrix = []
     
-	for i in range(np.size(methylation_data[:,0])):
-            y_true = methylation_data[i,].T
-            reg = LinearRegression().fit(meta_data, y_true)
-            intercept_and_coef = list(reg.coef_)
-            intercept_and_coef.insert(0, float(reg.intercept_))
-            coefficient_matrix.append(intercept_and_coef)
-            print i
+for i in range(np.size(methylation_data[:,0])):
+    y_true = methylation_data[i,].T
+    reg = Ridge()
+    reg = reg.fit(meta_data, y_true)
+    intercept_and_coef = list(reg.coef_)
+    intercept_and_coef.insert(0, float(reg.intercept_))
+    coefficient_matrix.append(intercept_and_coef)
+    if i%100000 == 0:
+        print i
 
-	with open("intercept_and_coefficients.csv", "wb") as f:
-		writer = csv.writer(f)
-		writer.writerows(coefficient_matrix)
 
-if __name__ == '__main__':
-	main()
+# In[5]:
+
+
+with open("intercept_and_coefficients.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(coefficient_matrix)
