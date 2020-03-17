70879865eba10b16bc5a0d43d34e9a7250c25da1
swebster777 <53287866+swebster777@users.noreply.github.com>
2020-03-17 02:18:38 -0700

    Update linear_regression_output_prediction.py

--- linear_regression_output_prediction.py
+++ linear_regression_output_prediction.py
@@ -20,20 +20,19 @@ def load_csv(filename):
 
 # In[4]:
 
-
+#load meta data and coefficients
 meta_data = np.array(load_csv('meta_data_no_labels_6var.csv'), dtype=np.float64)
 coefficient_matrix = np.array(load_csv('intercept_and_coefficients.csv'), dtype=np.float64)
 
 
 # In[7]:
 
-
 predictions = []
 
 for i in range(np.size(coefficient_matrix[:,0])):
-    if i%100000 == 0:
+    if i%100000 == 0: #progress report
         print i
-    intercept = coefficient_matrix[i][0]
+    intercept = coefficient_matrix[i][0] #extract intercept and coefficients for each site
     co_female = coefficient_matrix[i][1]
     co_cancer = coefficient_matrix[i][2]
     co_male_no_cancer = coefficient_matrix[i][3]
@@ -41,7 +40,7 @@ for i in range(np.size(coefficient_matrix[:,0])):
     co_female_no_cancer = coefficient_matrix[i][5]
     co_female_cancer = coefficient_matrix[i][6]
     predictions_for_site = []
-    for k in range(np.size(meta_data[:,0])):
+    for k in range(np.size(meta_data[:,0])): #make predictions for each patients based on their meta data
         predictions_for_site.append(intercept + co_female*meta_data[k][0] + co_cancer*meta_data[k][1] + co_male_no_cancer*meta_data[k][2] + co_male_cancer*meta_data[k][3] + co_female_no_cancer*meta_data[k][4] + co_female_cancer*meta_data[k][5])
     predictions.append(predictions_for_site)                   
             
